42e1eaa64a2016a2b167cab0e0e234e8c7f72611
ingresa tu nombre <test@test.com>
2025-08-18 23:03:40 -0500

    mejora en las consultas

--- app/services/RegistrosPagosServices.ts
+++ app/services/RegistrosPagosServices.ts
@@ -1,38 +1,63 @@
 import RegistrosPago from '#models/registros_pago'
+import { PostRegistroPago } from '../interfaces/registros_pagos.js'
 
 export default class RegistrosPagoService {
-async get_all_pagos() {
-  return await RegistrosPago.query()
-    .preload('formaPago')
-    .preload('membresia', (membresiaQuery) => {
-      membresiaQuery.preload('membresiaPaciente', (mxpQuery) => {
-        mxpQuery.preload('paciente', (pacienteQuery) => {
-          pacienteQuery.preload('usuario')
-        })
+  async get_all_pagos() {
+    return await RegistrosPago.query()
+      .select()
+      .preload('formaPago', (fpquery)=>fpquery.select('tipo_pago'))
+      .preload('membresia', (membresiaQuery) => {
+        membresiaQuery
+          .select('numero_contrato')
+          .preload('membresiaPaciente', (mxpQuery) => {
+            mxpQuery.preload('paciente', (pacienteQuery) => {
+              pacienteQuery.select('direccion_cobro','usuario_id')
+              .preload('usuario', (usuarioQuery) => {
+                usuarioQuery.select(
+                  'id_usuario',
+                  'nombre',
+                  'segundo_nombre',
+                  'apellido',
+                  'segundo_apellido',
+                  'email'
+                )
+              })
+            })
+          })
       })
-    })
-}
-
+  }
 
   async get_pago_id(id: number) {
     return await RegistrosPago.query()
       .where('id_registro', id)
-      .preload('formaPago')
-      .preload('membresia',(membresiaQuery) => {
-      membresiaQuery.preload('membresiaPaciente', (mxpQuery) => {
-        mxpQuery.preload('paciente', (pacienteQuery) => {
-          pacienteQuery.preload('usuario')
-        })
+      .preload('formaPago', (fpquery)=>fpquery.select('tipo_pago'))
+      .preload('membresia', (membresiaQuery) => {
+        membresiaQuery
+          .select('numero_contrato')
+          .preload('membresiaPaciente', (mxpQuery) => {
+            mxpQuery.preload('paciente', (pacienteQuery) => {
+              pacienteQuery.select('direccion_cobro','usuario_id')
+              .preload('usuario', (usuarioQuery) => {
+                usuarioQuery.select(
+                  'id_usuario',
+                  'nombre',
+                  'segundo_nombre',
+                  'apellido',
+                  'segundo_apellido',
+                  'email'
+                )
+              })
+            })
+          })
       })
-    })
-    .firstOrFail()
+      .firstOrFail()
   }
 
-  async create_pago(data: any) {
+  async create_pago(data: PostRegistroPago) {
     return await RegistrosPago.create(data)
   }
 
-  async update_pago(id: number, data: any) {
+  async update_pago(id: number, data: PostRegistroPago) {
     const registro = await RegistrosPago.findOrFail(id)
     registro.merge(data)
     await registro.save()
