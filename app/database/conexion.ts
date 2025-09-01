20b4b47ab067a3543f4942a32577f4db829b28de
ingresa tu nombre <test@test.com>
2025-09-01 00:48:38 -0500

    arreglo en la conexion a la db

--- app/database/conexion.ts
+++ app/database/conexion.ts
@@ -3,7 +3,7 @@ import {Client} from 'pg';
 export const client = new Client({
     port:Number(process.env.DB_PORT), //parseo a number y no genere error
     user:process.env.DB_MOTOR,
-    host:process.env.HOST,
+    host:process.env.DB_HOST,
     database:process.env.DB_DATABASE,
     password:process.env.DB_PASSWORD
 })
