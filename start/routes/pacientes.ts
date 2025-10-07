f4f3f7fa152ab7022ca863808a56d0ec4ca31df5
parvical258 <andreslasso250@gmail.com>
2025-10-07 15:02:55 -0500

    hotFix beneficiarios

--- start/routes/pacientes.ts
+++ start/routes/pacientes.ts
@@ -1,14 +1,26 @@
-import PacientesController from "#controllers/PacientesController";
-import router from "@adonisjs/core/services/router";
+import PacientesController from '#controllers/PacientesController'
+import router from '@adonisjs/core/services/router'
 
-const paciente = new PacientesController();
+const paciente = new PacientesController()
 
-router.post('/pacientes', paciente.create);
-router.get('/pacientes', paciente.readAll);
-router.get('/pacientes/titular', paciente.readByITitular);
-router.get('/pacientes/:id', paciente.readById);
-router.delete('/pacientes/:id', paciente.deleteById);
-router.put('/pacientes/:id', paciente.updateById);
-router.get('/pacientes/mi-perfil', paciente.readByUsuarioLogueado);
-router.get('/pacientes/por-usuario/:usuario_id', paciente.readByUsuarioId);
+//  crear paciente
+router.post('/pacientes', paciente.create)
+
+//  listar todos los pacientes
+router.get('/pacientes', paciente.readAll)
+
+//  listar todos los beneficiarios
 router.get('/pacientes/beneficiarios', paciente.readBeneficiarios)
+
+//  listar todos los titulares
+router.get('/pacientes/titular', paciente.readByITitular)
+
+// listar paciente del usuario autenticado
+router.get('/pacientes/mi-perfil', paciente.readByUsuarioLogueado)
+
+// Buscar paciente por usuario_id (por beneficiario ojo)
+router.get('/pacientes/por-usuario/:usuario_id', paciente.readByUsuarioId)
+
+router.get('/pacientes/:id', paciente.readById)
+router.put('/pacientes/:id', paciente.updateById)
+router.delete('/pacientes/:id', paciente.deleteById)
